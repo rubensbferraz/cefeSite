a475d04a01e725f9ee56e6066897584559212de1
Carlos Rubens Bezerra Ferraz <rubensbferraz@gmail.com>
2018-05-19 14:24:54 -0300

    Atualização 19/05/2018

--- cefe-app/src/component/Cabecalho/cabecalho.js
+++ cefe-app/src/component/Cabecalho/cabecalho.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import Menu from './menu.js'
+import Menu from './menu.js';
 import './css/cabecalho.css';
-import 'flexbox-react'
+import Flexbox from 'flexbox-react';
 
 class Cabecalho extends Component{
     render(){
         return(
-            <div className="cabecalho">
-                <div className="app-cabecalho col-xs-12">
-                    <div className="logo col-xs-6">
+            <Flexbox className="cabecalho">
+                <Flexbox className="app-cabecalho col-xs-12">
+                    <Flexbox className="logo col-xs-6">
                         <img src="../../../../images/CEFELogo.png" alt='' id="logoCEFE" />
                         <span id="textologoCEFE" >CEFE<p>Casa Espírita Fratenidade Emanuel</p></span>
-                    </div>
-                    <div className="fundoGiraSol col-xs-12">
+                    </Flexbox>
+                    <Flexbox className="fundoGiraSol col-xs-12">
                         <img alt='' src="../../../../images/fundoCabecalhoGiraSois.png" />
-                    </div>
-                </div>
+                    </Flexbox>
+                </Flexbox>
                 <Menu />
-            </div>        
+            </Flexbox>        
         );
     }
 }
