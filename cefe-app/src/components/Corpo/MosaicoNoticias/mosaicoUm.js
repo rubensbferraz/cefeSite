83ac85d07ef74e3fdadfa806068c03616983a48e
carlos.ferraz <carlos.ferraz@eldorado.org.br>
2018-09-21 08:55:24 -0300

    atualização

---
+++ cefe-app/src/components/Corpo/MosaicoNoticias/mosaicoUm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class mosaicoUm extends Component {
+    render () {
+        return (
+            <div>
+                <h6>Aqui teremos Informações relevantes tópico um</h6>
+                <a href="/palestra">teste</a>
+            </div>
+        )
+    }
+}
+
+export default mosaicoUm
